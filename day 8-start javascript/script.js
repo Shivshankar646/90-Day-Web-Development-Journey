80ac81167deed62ccc8bc61d36aee0f6642e5ee3
Shivshankar646 <shivshankarmunde91@gmail.com>
2026-09-23 16:23:05 +0530

    today i have practiced some problems related to javascript basics

--- day 8-start javascript/script.js
+++ day 8-start javascript/script.js
@@ -1,8 +1,68 @@
-let name = "shivshankar";
-let age = 22;
-let isLearningJavaScript = true;
-console.log("Name:",name);
-console.log("Age:",age);
-console.log("learning javascript:",isLearningJavaScript);
+// // let name = "shivshankar";
+// // let age = 22;
+// let isLearningJavaScript = true;
+// console.log("Name:",name);
+// // console.log("Age:",age);
+// console.log("learning javascript:",isLearningJavaScript);
 
+// const birthYear = 2004;
+// const collegeName = "MGMS College of Engineering Nanded";
+// const country = "india";
 
+// console.log(birthyear);
+// console.log(collegename);
+// console.log(country);
+
+// let name = "shivshankar";
+// let age = 22;
+// let isStudent =true;
+// let bhai;
+// let sallary = null;
+// console.log(typeof(name));
+// console.log(typeof(age));
+// console.log(typeof(isStudent));
+// console.log(typeof(bhai));
+// console.log(typeof(salary));
+
+// let price =750;
+// let quantity =4;
+// let total =price * quantity;
+// let priceAfterDeliveryCharge = total + 100;
+// let priceAfterDiscount = priceAfterDeliveryCharge - 200;
+// console.log("Total price:",total);
+// console.log("Price after adding ₹100 delivery charge:",total + 100);
+// console.log("Price after giving ₹200 discount:",priceafterdiscount)
+
+// let productPrice = 120;
+// let quantity = 3;
+// let deliveryCharge = 80;
+// let discount = 250;
+// let subtotal = ProductPrice * Quantity;
+// let finalPrice = (subtotal + DeliveryCharge) - Discount;
+// let howMuchSaves =Discount;
+// console.log("Subtotal:",subtotal);
+// console.log("Final price:",finalPrice);
+// console.log("customer saves:",howMuchSaves);
+
+// let Age = 22;
+// console.log(18< Age);
+// console.log(18 == Age);
+// console.log(25>= Age);
+// let age =22;
+// let id = true;
+// console.log(age ==18 || age > 18 && id == true );
+
+// let name = "Shivshankar";
+// let age = 22;
+// let college = "Mgm's College of Engineering , Nanded";
+// console.log(`My name is ${name}, I am ${age} years old, and I study at ${college}`);
+let price = 1200;
+let quantity = 2;
+let deliveryCharge = 100;
+let discount = 150;
+
+let subtotal = price * quantity;
+let finalPrice = subtotal + deliveryCharge - discount;
+let amountSaved = discount;
+
+console.log(`Laptop Bag × ${quantity} → Final price: ₹${finalPrice}, You saved: ₹${amountSaved}`);
---
+++ day 8-start javascript/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+// let name = "shivshankar";
+// // let age = 22;
+// let isLearningJavaScript = true;
+// console.log("Name:",name);
+// // console.log("Age:",age);
+// console.log("learning javascript:",isLearningJavaScript);
+
+// const birthYear = 2004;
+// const collegeName = "MGMS College of Engineering Nanded";
+// const country = "india";
+
+// console.log(birthyear);
+// console.log(collegename);
+// console.log(country);
