16766d883b78360f65c17b3aaa64b6202e4a6d4e
Shivshankar646 <shivshankarmunde91@gmail.com>
2026-09-23 11:15:21 +0530

    started learning javascript

---
+++ day 8-start javascript/script.js
@@ -0,0 +1,8 @@
+let name = "shivshankar";
+let age = 22;
+let isLearningJavaScript = true;
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("learning javascript:",isLearningJavaScript);
+
+
